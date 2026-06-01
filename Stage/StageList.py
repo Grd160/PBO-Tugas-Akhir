325943e4d960d45b9870547f21d539611441b84e
Garda <rchnch2807@gmail.com>
2026-06-01 20:03:59 +0700

    stagelist

--- Stage/StageList.py
+++ Stage/StageList.py
@@ -1,6 +1,6 @@
 from Stage.Stage1 import Stage1
 from Stage.Stage2 import Stage2 
-from stage.stage3 import Stage3
+from Stage.Stage3 import Stage3
 
 # Daftar semua stage yang tersedia
 ALL_STAGES = [
