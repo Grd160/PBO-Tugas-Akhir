ed37926b9578392cba71a917f01117159f1b2285
Arga <25051204183@mhs.unesa.ac.id>
2026-06-01 19:40:32 +0700

    Update StageList.py

--- Stage/StageList.py
+++ Stage/StageList.py
@@ -1,8 +1,10 @@
 from Stage.Stage1 import Stage1
 from Stage.Stage2 import Stage2 
+from stage.stage3 import Stage3
 
 # Daftar semua stage yang tersedia
 ALL_STAGES = [
     Stage1,
     Stage2,  
+    Stage3,
 ]
